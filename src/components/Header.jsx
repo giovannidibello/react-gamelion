3c795762131c883e4c77041080151bb24bdbcf2b
Giovanni <dibellogio@gmail.com>
2025-06-13 19:46:55 +0200

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,13 @@
 // Header.jsx
 
-// importo il modulo per il link
-import { Link } from "react-router-dom"
-
-// importo fontawesome
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faClapperboard } from '@fortawesome/free-solid-svg-icons';
-
 export default function Header() {
 
     return (
 
-        <nav className="navbar navbar-expand-md navbar-light shadow">
+        <nav className="navbar shadow">
             <div className="d-flex justify-content-start container-fluid">
                 <div className="logo">
-                    <Link className="navbar-brand" to="/">
-                        <img src="/gamelion-logo.png" alt="Gamelion Logo" />
-                    </Link>
+                    <img src="/gamelion-logo.png" alt="Gamelion Logo" />
                 </div>
                 <div className="title">
                     <img src="/gamelion-text.png" alt="Gamelion Title" />
